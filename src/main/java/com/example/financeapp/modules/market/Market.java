f19412b3e93e86fae2f9ddedd0e552224c552b6e
Christoph Fischer <christoph.fischer@hpe.com>
2022-01-12 16:54:58 +0100

    Implementation of simple functions

--- src/main/java/com/example/financeapp/modules/market/Market.java
+++ src/main/java/com/example/financeapp/modules/market/Market.java
@@ -1,4 +1,30 @@
 package com.example.financeapp.modules.market;
 
+import com.example.financeapp.modules.share.Share;
+
+import java.util.List;
+
 public class Market {
+
+    //attributes
+    private int totalShareAmount;
+    private List<Share> shareList;
+
+    public Market() {
+
+    }
+
+    public boolean buy(long shareId, int amount) {
+        boolean result = false;
+        return result;
+    }
+
+    public boolean sell(long shareId, int amount) {
+        boolean result = false;
+        return result;
+    }
+
+    public void initializeMarket() {
+
+    }
 }
