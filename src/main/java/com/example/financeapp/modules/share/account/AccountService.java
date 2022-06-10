534953463c567ede7133b62a5de3a73f28b24c4b
ChrisDHBW <christophfischerdhbw@gmail.com>
2022-06-10 12:16:31 +0200

    Deleted useless account classes

--- src/main/java/com/example/financeapp/modules/share/account/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.example.financeapp.modules.share.account;
-
-import java.util.List;
-
-public interface AccountService {
-
-    List<Account> findAllAccounts();
-    Account findMyAccount(String email);
-}
