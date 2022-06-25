9eb76915c6db3225732d334ac8c9abe26e9cdab8
HendrikWeisgerber <57496555+HendrikWeisgerber@users.noreply.github.com>
2022-06-25 17:55:53 +0200

    lauffähig

---
+++ src/main/java/com/example/financeapp/modules/user/communication/models/UserCreateCommunicationModel.java
@@ -0,0 +1,24 @@
+package com.example.financeapp.modules.user.communication.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NotNull
+public class UserCreateCommunicationModel {
+
+    private String email;
+    private String name;
+    private String surname;
+    private int phoneNumber;
+    private String street;
+    private String houseNumber;
+    private String postalCode;
+    private String city;
+    private String country;
+    private String birthDate;
+    private String taxNumber;
+}
